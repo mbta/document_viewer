7bb3e4f0d8bb83cdd7187b2ea2e280820f215ac5
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2021-03-19 15:23:57 -0400

    Remove unused download_file_fn and stream_fn options

--- lib/s3/binary_stream.ex
+++ lib/s3/binary_stream.ex
@@ -6,13 +6,11 @@ defmodule S3.BinaryStream do
   @doc """
   Returns an S3 object as a stream of binary chunks.
   """
+  @spec binary_chunks(String.t(), String.t()) :: Enumerable.t()
   @spec binary_chunks(String.t(), String.t(), ExAws.S3.download_file_opts()) :: Enumerable.t()
   def binary_chunks(bucket, file, opts \\ []) do
-    download_file_fn = Keyword.get(opts, :download_file_fn, &ExAws.S3.download_file/4)
-    stream_fn = Keyword.get(opts, :stream_fn, &ExAws.stream!/1)
-
     bucket
-    |> download_file_fn.(file, :memory, opts)
-    |> stream_fn.()
+    |> ExAws.S3.download_file(file, :memory, opts)
+    |> ExAws.stream!()
   end
 end
