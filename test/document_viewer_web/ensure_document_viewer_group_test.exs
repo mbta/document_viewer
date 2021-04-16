d0c98244057930d3e24a827a7d859e8fac76415f
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2021-04-16 16:22:08 -0400

    Get default opts rather than hard coding

--- test/document_viewer_web/ensure_document_viewer_group_test.exs
+++ test/document_viewer_web/ensure_document_viewer_group_test.exs
@@ -3,6 +3,8 @@ defmodule DocumentViewerWeb.EnsureDocumentViewerGroupTest do
 
   alias DocumentViewerWeb.EnsureDocumentViewerGroup
 
+  @default_opts EnsureDocumentViewerGroup.init([])
+
   describe "init/1" do
     test "passes options through with the addition of the cognito group" do
       assert EnsureDocumentViewerGroup.init([]) == [document_viewer_group: "document-viewer"]
@@ -12,13 +14,12 @@ defmodule DocumentViewerWeb.EnsureDocumentViewerGroupTest do
   describe "call/2" do
     @tag :authenticated
     test "does nothing when user is in the document-viewer group", %{conn: conn} do
-      assert conn ==
-               EnsureDocumentViewerGroup.call(conn, document_viewer_group: "document-viewer")
+      assert conn == EnsureDocumentViewerGroup.call(conn, @default_opts)
     end
 
     @tag :authenticated_not_in_group
     test "redirects when user is not in the document-viewer group", %{conn: conn} do
-      conn = EnsureDocumentViewerGroup.call(conn, document_viewer_group: "document-viewer")
+      conn = EnsureDocumentViewerGroup.call(conn, @default_opts)
 
       assert redirected_to(conn) == "https://www.mbta.com"
     end
