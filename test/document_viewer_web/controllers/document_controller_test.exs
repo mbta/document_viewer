0d2bea28994cd40fe714756c1ac4a5b57d6c264f
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2021-04-21 15:56:23 -0400

    Log all user actions (#9)
    • login
    • search
    • view document
    • download document

--- test/document_viewer_web/controllers/document_controller_test.exs
+++ test/document_viewer_web/controllers/document_controller_test.exs
@@ -1,12 +1,33 @@
 defmodule DocumentViewerWeb.DocumentControllerTest do
   use DocumentViewerWeb.ConnCase
+  import ExUnit.CaptureLog
+  import Test.Support.Helpers
+
+  @moduletag capture_log: true
 
   describe "show: GET /documents/:bucket_name/:file_path" do
+    setup do
+      reassign_log_level(:info)
+    end
+
     @tag :authenticated
     test "displays a search page", %{conn: conn} do
       conn = get(conn, "/documents/BUCKET/FILE.pdf")
 
       assert html_response(conn, 200) =~ "FILE.pdf"
+    end
+
+    @tag :authenticated
+    test "logs that the user viewed the document", %{conn: conn} do
+      log =
+        capture_log(fn ->
+          get(conn, "/documents/BUCKET/FILE.pdf")
+        end)
+
+      assert log =~ "username=\"test_user\""
+      assert log =~ "action=:view_document"
+      assert log =~ "bucket_name=\"BUCKET\""
+      assert log =~ "file_path=\"FILE.pdf\""
     end
 
     test "unauthenticated, redirects you to cognito auth", %{conn: conn} do
@@ -26,6 +47,10 @@ defmodule DocumentViewerWeb.DocumentControllerTest do
   end
 
   describe "pdf: GET /documents/:bucket_name/:file_path/pdf" do
+    setup do
+      reassign_log_level(:info)
+    end
+
     @tag :authenticated
     test "redirects you to the desired document", %{conn: conn} do
       redirect_url = "https://example.com/BUCKET/FILE.pdf"
@@ -43,6 +68,28 @@ defmodule DocumentViewerWeb.DocumentControllerTest do
         |> get("/documents/BUCKET/FILE.pdf/pdf")
 
       assert redirected_to(conn) == redirect_url
+    end
+
+    @tag :authenticated
+    test "download requested, logs that the user downloaded the document", %{conn: conn} do
+      config_fn = fn _ -> %{} end
+
+      presigned_url_fn = fn _, _, _, _, _ ->
+        {:ok, "https://example.com/BUCKET/FILE.pdf"}
+      end
+
+      log =
+        capture_log(fn ->
+          conn
+          |> assign(:config_fn, config_fn)
+          |> assign(:presigned_url_fn, presigned_url_fn)
+          |> get("/documents/BUCKET/FILE.pdf/pdf?download=true")
+        end)
+
+      assert log =~ "username=\"test_user\""
+      assert log =~ "action=:download_document"
+      assert log =~ "bucket_name=\"BUCKET\""
+      assert log =~ "file_path=\"FILE.pdf\""
     end
 
     test "unauthenticated, redirects you to cognito auth", %{conn: conn} do
