bfd1b69deb29e1fa7e76b4e8b4eb0d22ca8729fc
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2021-05-24 16:13:17 -0400

    Fix prod endpoint config, run a server

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,10 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :document_viewer, DocumentViewerWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  http: [:inet6, port: System.get_env("PORT") || 4000],
+  url: [host: {:system, "HOST"}, port: 80],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true
 
 # Do not print debug messages in production
 config :logger, level: :info
