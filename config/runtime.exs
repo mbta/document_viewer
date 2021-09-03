22655766d32dd7d038d6f7ed55320f2ad588ff4a
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2021-09-03 13:55:59 -0400

    fix: Make the env setting clearer
    Co-authored-by: Paul Swartz <pswartz@mbta.com>

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,5 +1,6 @@
 import Config
 
-config :document_viewer,
-  api_auth_token:
-    System.get_env("API_AUTH_TOKEN", Application.get_env(:document_viewer, :api_auth_token))
+if token = System.get_env("API_AUTH_TOKEN") do
+  config :document_viewer,
+    api_auth_token: token
+end
